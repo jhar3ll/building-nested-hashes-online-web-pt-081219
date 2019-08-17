35bb2deb99ebff31ab0a42c446b15ab3e7897059
Jharell Chambers <jharell.chambers@selu.edu>
2019-08-17 05:19:32 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -26,8 +26,33 @@ def first_challenge
           }
         ]
        },
-       :capulet => {
-         :patriarch =>
-       }
-   }
+        :capulet => {
+    :patriarch => {
+      :name => "Lord Capulet",
+      :age => "50"
+    },
+    :matriarch => {
+      :name => "Lady Capulet",
+      :age => "51"
+    },
+    :heroine => {
+      :name => "Juliet",
+      :age => "15",
+      :status => "alive"
+    },
+    :heroine_friends=> [
+      {
+        :name => "Steven",
+        :age => "30",
+        :attitude => "confused"
+      },
+      {
+        :name => "Nurse",
+        :age => "44",
+        :attitude => "worried"
+      }
+    ]
+  }
+}
+
 end
